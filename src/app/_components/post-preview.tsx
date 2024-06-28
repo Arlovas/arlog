a753da7f11279e0c5d929fb14ae93cf04a971b24
Artur Comunello <artur.comunello@alludo.com>
2024-06-28 12:38:43 -0300

    Starting a new post

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -28,12 +28,12 @@ export function PostPreview({
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <h3 className="text-3xl leading-snug">
+      <h3 className="text-3xl mb-2 leading-snug">
         <Link href={`/posts/${slug}`} className="hover:underline">
           {title}
         </Link>
       </h3>
-      <p className="text-lg leading-relaxed mb-6">{excerpt}</p>
+      <p className="text-lg bg-gray-50 rounded-md p-2 leading-relaxed mb-6">{excerpt}</p>
       <Avatar name={author.name} picture={author.picture} date={date} dateFormat={dateFormat} />
     </div>
   );
